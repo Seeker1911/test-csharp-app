51645f43cd860fe8993e2904b04d532951e0edda
seeker1911 <michaelmead007@icloud.com>
2019-12-29 12:50:03 -0600

    test c sharp app

---
+++ AccountApp/Account.cs
@@ -0,0 +1,29 @@
+namespace AccountApp
+{
+    public class Account
+    {
+        private string  name; // instance variable
+ 
+        // method that sets the account name in the object
+        public void  SetName(string  accountName)         
+        {                                                 
+            name = accountName; // store the account name  
+        }                                                 
+ 
+        // method that retrieves the account name from the object       
+        public string  GetName()                                        
+        {                                                               
+            return  name; // returns name's value to this method's caller
+        }                                                               
+    }
+
+    public class ConstructorClass
+    {
+        public string Name { get; set; }
+
+        public ConstructorClass(string accountName)
+        {
+            Name = accountName;
+        }
+    }
+}
